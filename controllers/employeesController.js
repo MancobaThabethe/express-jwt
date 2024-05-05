7f617ef65198e3ff78e3e070301d21a1df776aed
terriisx@gmail.com <terriisx@gmail.com>
2024-05-05 04:25:26 +0200

    connected to DB

--- controllers/employeesController.js
+++ controllers/employeesController.js
@@ -1,17 +1,13 @@
-const data = {
-    employees: require('../model/employees.json'),
-    setEmployees: function (data) { this.employees = data }
-}
-const fsPromises = require('fs').promises
-const path = require('path')
+const Employee = require('../model/Employee')
 
-const getAllEmployees = (req, res) => {
-    res.json(data.employees);
+const getAllEmployees = async (req, res) => {
+    const Employees = await Employee.find()
+    if(!Employees) return res.sendStatus(204).json({msg: 'No employees found'})
+    res.json(Employees);
 }
 
 const createNewEmployee = async (req, res) => {
     const newEmployee = {
-        id: data.employees?.length ? data.employees[data.employees.length - 1].id + 1 : 1,
         firstname: req.body.firstname,
         lastname: req.body.lastname
     }
@@ -20,40 +16,40 @@ const createNewEmployee = async (req, res) => {
         return res.status(400).json({ 'message': 'First and last names are required.' });
     }
 
-    data.setEmployees([...data.employees, newEmployee]);
-    await fsPromises.writeFile(
-        path.join(__dirname, '..', 'model', 'employees.json'),
-        JSON.stringify(data.employees)
-    );
+    await Employee.create(newEmployee)
     
-    res.status(201).json(data.employees);
+    res.status(201).json({ 'success': `New employee ${newEmployee.firstname} created!` });
 }
 
-const updateEmployee = (req, res) => {
-    const employee = data.employees.find(emp => emp.id === parseInt(req.body.id));
+const updateEmployee = async (req, res) => {
+    if(req?.body.id) return res.status(400).json({ "message": `Employee ID parameter required` })
+    
+    const employee = await Employee.findOne({_id: req.body._id}).exec()
     if (!employee) {
-        return res.status(400).json({ "message": `Employee ID ${req.body.id} not found` });
+        return res.status(400).json({ "message": `Employee ID ${req.body._id} not found` });
     }
     if (req.body.firstname) employee.firstname = req.body.firstname;
     if (req.body.lastname) employee.lastname = req.body.lastname;
-    const filteredArray = data.employees.filter(emp => emp.id !== parseInt(req.body.id));
-    const unsortedArray = [...filteredArray, employee];
-    data.setEmployees(unsortedArray.sort((a, b) => a.id > b.id ? 1 : a.id < b.id ? -1 : 0));
-    res.json(data.employees);
+    await employee.save()
+
+    res.json({"message": `Employee ID ${req.body._id} details updated.` });
 }
 
-const deleteEmployee = (req, res) => {
-    const employee = data.employees.find(emp => emp.id === parseInt(req.body.id));
+const deleteEmployee = async (req, res) => {
+    if(req?.body.id) return res.status(400).json({ "message": `Employee ID parameter required` })
+    const employee = await Employee.findOne({'_id': req.body._id}).exec()
     if (!employee) {
-        return res.status(400).json({ "message": `Employee ID ${req.body.id} not found` });
+        return res.status(400).json({ "message": `Employee ID ${req.body._id} not found` });
     }
-    const filteredArray = data.employees.filter(emp => emp.id !== parseInt(req.body.id));
-    data.setEmployees([...filteredArray]);
+    
+    await Employee.deleteOne({_id: req.body._id})
+
     res.json(data.employees);
 }
 
-const getEmployee = (req, res) => {
-    const employee = data.employees.find(emp => emp.id === parseInt(req.params.id));
+const getEmployee = async(req, res) => {
+    if(req?.params.id) return res.status(400).json({ "message": `Employee ID parameter required` })
+        const employee = await Employee.findOne({_id: req.params.id}).exec()
     if (!employee) {
         return res.status(400).json({ "message": `Employee ID ${req.params.id} not found` });
     }
--- controllers/refreshTokenController.js
+++ controllers/refreshTokenController.js
@@ -1,17 +1,14 @@
-const usersDB = {
-    users: require('../model/users.json'),
-    setUsers: function (data) { this.users = data }
-}
+const User = require('../model/User')
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
-const handleRefreshToken = (req, res) => {
+const handleRefreshToken = async (req, res) => {
     const cookies = req.cookies;
     if (!cookies?.jwt) return res.sendStatus(401);
     const refreshToken = cookies.jwt;
 
-    const foundUser = usersDB.users.find(person => person.refreshToken === refreshToken);
-    if (!foundUser) return res.sendStatus(403); //Forbidden 
+    const foundUser = await User.findOne({username: refreshToken}).exec(); // .exec() is an aync function
+    if (!foundUser) return res.sendStatus(403); //Forbidden
     // evaluate jwt 
     jwt.verify(
         refreshToken,
